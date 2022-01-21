ba7680813dd478cde55928d4c9176333ea89f2ca
Mario <mariomass23@gmail.com>
2022-01-21 18:31:54 -0300

    nuevo estilo y js

--- script.js
+++ script.js
@@ -1,12 +1,74 @@
-const btn = document.querySelectorAll('button');
-const visor = document.getElementById('visor');
-var arreglo = [];
-let seleccion;
+let cuenta = document.getElementById('cuenta');
+let resultado = document.getElementById('resultado');
+let button = Array.from(document.getElementsByClassName('button'));
 
-btn.forEach(seleccionBtn => seleccionBtn.addEventListener('click', (e) => {
-    seleccion = e.target.innerText;
-    arreglo.push(seleccion);
-    visor.innerHTML = arreglo;
-    console.log(arreglo);
+button.map(buttons => {
+    buttons.addEventListener('click', (e) =>{
+        switch (e.target.innerText) {
+         case 'AC':
+             cuenta.innerText = '';
+             resultado.innerText = '';
+             break;
+         case 'DEL':
+             if (cuenta.innerText) {
+                 cuenta.innerText = cuenta.innerText.slice(0, -1);
+             }
+             break;
+        case '*':
+            if (cuenta.innerText !== '' || resultado.innerText !== '') {
+                cuenta.innerText += e.target.innerText;
+            }    
+            break; 
+        case '/':
+            if (cuenta.innerText !== '' || resultado.innerText !== '') {
+                cuenta.innerText += e.target.innerText;
+            }    
+            break; 
+        case '+':
+            if (cuenta.innerText !== '' || resultado.innerText !== '') {
+                cuenta.innerText += e.target.innerText;
+            }    
+            break; 
+        case '-':
+            if (cuenta.innerText !== '' || resultado.innerText !== '') {
+                cuenta.innerText += e.target.innerText;
+            }    
+            break; 
+         case '=':
+             if (cuenta.innerText !== '') {
+                let current = eval(cuenta.innerText);
+                let result = parseFloat(current);
+                let tolocaleString = result.toLocaleString('en');
+                resultado.innerText = tolocaleString;
+                cuenta.innerText = '';        
+             }
+             if (resultado.innerText !== ''){
+                switch (resultado.innerText) {
+                    case '*':
+                       resultado.innerText = resultado.innerText * cuenta.innerText;
+                        break;
+                
+                    default:
+                        break;
+                }
+             }
+             break;
+             default:
+                cuenta.innerText += e.target.innerText;
+                // let current = parseFloat(cuenta.innerText);
+                // if(isNaN(current)) return '.'
+                // let toLocaleString = current.toLocaleString('en');
+                // cuenta.innerText = toLocaleString;
+        }
+    })
+})
 
-}));
+
+// case '=':
+//     if (cuenta.innerText !== '') {
+//        let current = eval(cuenta.innerText);
+//        let result = parseFloat(current);
+//        let tolocaleString = result.toLocaleString('en');
+//        resultado.innerText = tolocaleString;
+//        cuenta.innerText = '';
+//     }
